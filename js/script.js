b4ba2e2a4cf14541e0e463c6cc5e69f5c1bcb6d2
RuhanPhelipe <ruhanphelipe.12@gmail.com>
2023-06-14 05:18:32 -0300

    add atividade

--- js/script.js
+++ js/script.js
@@ -3,29 +3,35 @@ const listaAnimais = [
     nome: "Animal 1",
     descricao:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam nisl, dapibus quis dictum quis, facilisis non risus.",
-    imgURL: "",
+    imgURL: "./img/section/cat-silhouette.png",
   },
   {
     nome: "Animal 2",
     descricao:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam nisl, dapibus quis dictum quis, facilisis non risus.",
-    imgURL: "",
+    imgURL: "./img/section/dog-silhouette.png",
   },
   {
     nome: "Animal 3",
     descricao:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam nisl, dapibus quis dictum quis, facilisis non risus.",
-    imgURL: "",
+    imgURL: "./img/section/cat-silhouette.png",
   },
   {
     nome: "Animal 4",
     descricao:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam nisl, dapibus quis dictum quis, facilisis non risus.",
-    imgURL: "",
+    imgURL: "./img/section/dog-silhouette.png",
+  },
+  {
+    nome: "Animal 5",
+    descricao:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin diam nisl, dapibus quis dictum quis, facilisis non risus.",
+    imgURL: "./img/section/cat-silhouette.png",
   },
 ];
 
-let container = document.querySelector("main .container");
+let container = document.querySelector("#animais .container");
 
 const criacaoPosts = () => {
   listaAnimais.forEach((animal) => {
